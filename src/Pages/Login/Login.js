3cc9f55061806d36e25a1e1286cc5f20e76c92eb
Marjan-001 <marjan199721@gmail.com>
2022-11-25 18:08:45 +0600

    sign up and login page design

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,40 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import log from '../../assets/images/Login.jpg'
 const Login = () => {
     return (
-        <div>
-            <h2>this is login page</h2>
-        </div>
+        <div className="hero  w-full my-20 ">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col  lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4 skew-y-6' src={log} alt='' />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 shadow-slate-800/50">
+                    <h1 className="text-2xl text-center font-bold">Login</h1>
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-ghost btn-outline" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p className='text-center mb-4'>Don't have an account <Link className='text-black font-bold btn-link' to="/signup">Sign Up</Link> </p>
+                    <div className="divider">OR</div>
+                    <button className='btn bt-sm btn-outline flex content-center w-3/4 mx-auto mb-10'>CONTINUE WITH GOOGLE</button>
+
+                </div>
+            </div>
+        </div >
     );
 };
 
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import log from '../../assets/images/Login.jpg'
 
 const SignUp = () => {
+    const { register, formState: { errors } } = useForm();
     return (
-        <div>
-            <h2>this is signup page</h2>
-        </div>
+        <div className="hero mx-auto  w-full my-20 ">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col  lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4 skew-y-6' src={log} alt='' />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 shadow-slate-800/50">
+                    <h1 className="text-2xl text-center font-bold">Sign up</h1>
+                    <form className="card-body">
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label"> <span className="label-text">Name</span></label>
+                            <input type="text" {...register("name", {
+                                required: "Name is Required"
+                            })} className="input input-bordered w-full max-w-xs" />
+                            {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label"> <span className="label-text">Email</span></label>
+                            <input type="email" {...register("email", {
+                                required: true
+                            })} className="input input-bordered w-full max-w-xs" />
+                            {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label"> <span className="label-text">Password</span></label>
+                            <input type="password" {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: "Password must be 6 characters long" },
+                                pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+                            })} className="input input-bordered w-full max-w-xs" />
+                            {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                        </div>
+                        <input className='btn btn-outline w-full mt-4' value="Sign Up" type="submit" />
+                        {/* {signUpError && <p className='text-red-600'>{signUpError}</p>} */}
+                    </form>
+                    <p className='text-center mb-4'>Already have an account? <Link className='text-black font-bold btn-link' to="/login">Log in</Link> </p>
+
+
+                </div>
+            </div>
+        </div >
     );
 };
 
